47a80569173cb0007512bed320e5066e5fa9e3f8
Dimitri <dimitriasaliba@gmail.com>
2020-11-02 11:14:13 -0500

    final version

--- src/WordSearch3D.java
+++ src/WordSearch3D.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Implements a 3-d word search puzzle program.
@@ -7,12 +10,14 @@ public class WordSearch3D {
 	public WordSearch3D () {
 	}
 
+	final Random rng = new Random();
+
 	/**
 	 * Searches for all the words in the specified list in the specified grid.
 	 * You should not need to modify this method.
 	 * @param grid the grid of characters comprising the word search puzzle
 	 * @param words the words to search for
-	 * @param a list of lists of locations of the letters in the words
+	 * @return a list of lists of locations of the letters in the words
 	 */
 	public int[][][] searchForAll (char[][][] grid, String[] words) {
 		final int[][][] locations = new int[words.length][][];
@@ -21,6 +26,7 @@ public class WordSearch3D {
 		}
 		return locations;
 	}
+
 
 	/**
 	 * Searches for the specified word in the specified grid.
@@ -31,8 +37,50 @@ public class WordSearch3D {
 	 */
 	public int[][] search (char[][][] grid, String word) {
 		// TODO: implement me
+		char currentLetter = word.charAt(0);
+		for (int x = 0; x < grid.length; x++) {
+			for (int y = 0; y < grid[x].length; y++) {
+				for (int z=0; z < grid[x][y].length; z++) {
+					if (grid[x][y][z]==currentLetter) {
+						//Create vectors for all directions words could go
+						for (int i = -1; i < 2; i++) {
+							for (int j = -1; j<2; j++) {
+								for (int k = -1; k < 2; k++) {
+									if (i == 0 && j == 0 && k == 0) continue;
+									int curX = x+i;
+									int curY = y+j;
+									int curZ = z+k;
+									//Step along vectors for each letter and return locations
+									for (int c = 1; c < word.length(); c++) {
+										if (curX < 0 || curY < 0 || curZ < 0 || curX >=grid.length || curY >= grid[x].length || curZ >= grid[x][y].length) break;
+										if (word.charAt(c) == grid[curX][curY][curZ]) {
+											//Run if match
+											if (c == word.length()-1) {
+												int[][] ret = new int[word.length()][];
+												for (int r = 0; r < ret.length; r++) {
+													ret[r] = new int[] {x+(r*i), y+(r*j), z+(r*k)};
+												}
+												return ret;
+											}
+											//Move to next space and letter
+											curX += i;
+											curY += j;
+											curZ += k;
+										} else {
+											break;
+										}
+									}
+								}
+							}
+						}
+
+					}
+				}
+			}
+		}
 		return null;
 	}
+
 
 	/**
 	 * Tries to create a word search puzzle of the specified size with the specified
@@ -45,8 +93,62 @@ public class WordSearch3D {
 	 * no satisfying grid could be found.
 	 */
 	public char[][][] make (String[] words, int sizeX, int sizeY, int sizeZ) {
-		// TODO: implement me
-		return null;
+        for (int a=0; a < 1000; a++){ //runs whole thing 1000 times
+            char[][][] grid = new char[sizeX][sizeY][sizeZ]; //creates empty grid
+            boolean cantPlaceWord = false; //helps determine whether or not to fill in empty spaces at the end
+            for (int b=0; b < words.length; b++){ //goes through each word in the list
+                boolean placedWord = false; //helps determine whether to break out of the outerLoop or not
+                outerLoop:
+                for (int c=0; c < 1000; c++){ //runs 1000 times
+                    int randomPositionX = rng.nextInt(sizeX);
+                    int randomPositionY = rng.nextInt(sizeY);
+                    int randomPositionZ = rng.nextInt(sizeZ);
+                    for (int i=-1; i < 2; i++){
+                        for (int j=-1; j < 2; j++){
+                            for (int k=-1; k < 2; k++){
+                                if (i==0 && j==0 && k==0) continue;
+                                if (grid[randomPositionX][randomPositionY][randomPositionZ] == 0
+                                        || grid[randomPositionX][randomPositionY][randomPositionZ] == words[b].charAt(0)){
+                                    for (int l = 1; l < words[b].length(); l++){ //goes through each letter in the rest of the word to see if they will fit in the given direction
+                                        if (randomPositionX + (l * i) >= sizeX || randomPositionY + (l * j) >= sizeY || randomPositionZ + (l * k) >= sizeZ //sees if the letter will fall outside the graph in the positive direction
+                                                || randomPositionX + (l * i) < 0 || randomPositionY + (l * j) < 0 || randomPositionZ + (l * k) < 0){       //sees if the letter will fall outside the graph in the negative direction
+                                            break; //if any of those is true, the letter cannot fit, so we break out of the smaller loop while keeping placedWord = false
+                                        }else if (!(grid[randomPositionX + (l * i)][randomPositionY + (l * j)][randomPositionZ + (l*k)] == 0                      //sees if the position the letter will fall at is empty
+                                                || grid[randomPositionX + (l * i)][randomPositionY + (l * j)][randomPositionZ + (l*k)] == words[b].charAt(l))) {  //sees if the position the letter will fall at already has that letter in it
+                                            break; //if neither of those is true, the letter cannot fit, so we break out of the smaller loop while keeping placedWord = false
+                                        }else if (l == words[b].length() - 1){ //if we've reached the last letter, and have not broken out of the loop, then the word can fit
+                                            placedWord = true;
+                                            for (int d = 0; d < words[b].length(); d++){ //this loop places the word in the location
+                                                grid[randomPositionX + (d * i)][randomPositionY + (d * j)][randomPositionZ + (d * k)] = words[b].charAt(d);
+                                            }
+                                            break outerLoop;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (placedWord) break;
+                }
+                if (!placedWord){
+                    cantPlaceWord = true;
+                    break;
+                }
+            }
+            if (!cantPlaceWord) {
+                for (int i = 0; i < sizeX; i++){
+                    for (int j = 0; j < sizeY; j++){
+                        for (int k = 0; k < sizeZ; k++){
+                            if (grid[i][j][k] == 0){
+                                grid[i][j][k] = (char) (rng.nextInt(26) + 'a');
+                            }
+                        }
+                    }
+                }
+                return grid;
+            }
+        }
+	    return null;
 	}
 
 	/**
--- src/WordSearchTester.java
+++ src/WordSearchTester.java
@@ -22,31 +22,155 @@ public class WordSearchTester {
 		           (row[3] == 'j' && row[2] == 'a' && row[1] == 'v' && row[0] == 'a'));
 	}
 
+    @Test
+    /**
+     * Verifies that make can generate a simple 2D puzzle
+     */
+    public void testMake2D () {
+        final String[] words = new String[] { "java", "lisp" };
+        final char[][][] grid = _wordSearch.make(words, 1, 2, 4);
+        final char[][] row = grid[0];
+        assertTrue((row[0][0] == 'j' && row[0][1] == 'a' && row[0][2] == 'v' && row[0][3] == 'a'
+                                   && row[1][0] == 'l' && row[1][1] == 'i' && row[1][2] == 's' && row[1][3] == 'p') ||
+                           (row[0][3] == 'j' && row[0][2] == 'a' && row[0][1] == 'v' && row[0][0] == 'a'
+                                   && row[1][3] == 'l' && row[1][2] == 'i' && row[1][1] == 's' && row[1][0] == 'p') ||
+                           (row[0][0] == 'l' && row[0][1] == 'i' && row[0][2] == 's' && row[0][3] == 'p'
+                                   && row[1][0] == 'j' && row[1][1] == 'a' && row[1][2] == 'v' && row[1][3] == 'a') ||
+                           (row[0][3] == 'l' && row[0][2] == 'i' && row[0][1] == 's' && row[0][0] == 'p'
+                                   && row[1][3] == 'j' && row[1][2] == 'a' && row[1][1] == 'v' && row[1][0] == 'a') ||
+                           (row[0][0] == 'j' && row[0][1] == 'a' && row[0][2] == 'v' && row[0][3] == 'a'
+                                   && row[1][3] == 'l' && row[1][2] == 'i' && row[1][1] == 's' && row[1][0] == 'p') ||
+                           (row[0][3] == 'j' && row[0][2] == 'a' && row[0][1] == 'v' && row[0][0] == 'a'
+                                   && row[1][0] == 'l' && row[1][1] == 'i' && row[1][2] == 's' && row[1][3] == 'p') ||
+                           (row[0][0] == 'l' && row[0][1] == 'i' && row[0][2] == 's' && row[0][3] == 'p'
+                                   && row[1][3] == 'j' && row[1][2] == 'a' && row[1][1] == 'v' && row[1][0] == 'a') ||
+                           (row[0][3] == 'l' && row[0][2] == 'i' && row[0][1] == 's' && row[0][0] == 'p'
+                                   && row[1][0] == 'j' && row[1][1] == 'a' && row[1][2] == 'v' && row[1][3] == 'a'));
+    }
+
 	@Test
 	/**
 	 * Verifies that make returns null when it's impossible to construct a puzzle.
 	 */
 	public void testMakeImpossible () {
-		// TODO: implement me
+		final String[] words = new String[] { "java" };
+		final char[][][] grid = _wordSearch.make(words, 1, 1, 1);
+		assertTrue(grid==null);
 	}
 
-        @Test
-        /**
-         *  Verifies that search works correctly in a tiny grid that is effectively 2D.
-         */
-        public void testSearchSimple () {
-                // Note: this grid is 1x2x3 in size
-                final char[][][] grid = new char[][][] { { { 'a', 'b', 'c' },
-                                                           { 'd', 'f', 'e' } } };
-                final int[][] location = _wordSearch.search(grid, "be");
-                assertNotNull(location);
-                assertEquals(location[0][0], 0);
-                assertEquals(location[0][1], 0);
-                assertEquals(location[0][2], 1);
-                assertEquals(location[1][0], 0);
-                assertEquals(location[1][1], 1);
-                assertEquals(location[1][2], 2);
-        }
+    @Test
+    /**
+     * Verifies that make returns null when it's given an empty String array
+     */
+    public void testMakeEmpty() {
+        final String[] words = new String[] {};
+        final char[][][] grid = _wordSearch.make(words, 1, 1, 1);
+        assertNotNull(grid);
+    }
+
+    @Test
+    /**
+     * Verifies that make can make a test given two words that are the same, a palindrome, and has only enough room for one copy
+     */
+    public void testMakePalindrome () {
+        final String[] words = new String[] { "racecar", "racecar" };
+        final char[][][] grid = _wordSearch.make(words, 1, 1, 7);
+        final char[] row = grid[0][0];
+        assertTrue((row[0] == 'r' && row[1] == 'a' && row[2] == 'c' && row[3] == 'e' && row[4] == 'c' && row[5] == 'a' && row[6] == 'r'));
+    }
+
+    @Test
+    /**
+     * Verifies that make will return null when given numbers
+     */
+    public void testMakeSpecialCharacters () {
+        final String[] words = new String[] { "1", "2", "3" };
+        final char[][][] grid = _wordSearch.make(words, 1, 1, 3);
+        assertNull(grid);
+    }
+
+    @Test
+    /**
+     *  Verifies that search works correctly in a tiny grid that is effectively 2D.
+     */
+    public void testSearchSimple () {
+        // Note: this grid is 1x2x3 in size
+        final char[][][] grid = new char[][][] { { { 'a', 'b', 'c' },
+                { 'd', 'f', 'e' } } };
+        final int[][] location = _wordSearch.search(grid, "be");
+        assertNotNull(location);
+        assertEquals(location[0][0], 0);
+        assertEquals(location[0][1], 0);
+        assertEquals(location[0][2], 1);
+        assertEquals(location[1][0], 0);
+        assertEquals(location[1][1], 1);
+        assertEquals(location[1][2], 2);
+    }
+
+	@Test
+	/**
+	 *  Verifies that search works correctly in a tiny grid that is effectively 2D.
+	 */
+	public void testSearchSomeFail () {
+		// Note: this grid is 1x2x3 in size
+		final char[][][] grid = new char[][][] { { { 'a', 'b', 'c' },
+				{ 'd', 'f', 'e' } } };
+		final int[][][] locations = _wordSearch.searchForAll(grid, new String[] {"be", "ad", "steve gosling"});
+		assertNotNull(locations[0]);
+		assertNotNull(locations[1]);
+		assertNull(locations[2]);
+	}
+
+	@Test
+	/**
+	 *  Verifies that search works correctly when there is no match in a tiny grid that is effectively 2D.
+	 */
+	public void testSearchFailSimple () {
+		// Note: this grid is 1x2x3 in size
+		final char[][][] grid = new char[][][] { { { 'a', 'b', 'c' },
+				{ 'd', 'f', 'e' } } };
+		final int[][] location = _wordSearch.search(grid, "fizzbuzz");
+		assertNull(location);
+	}
+
+	@Test
+	/**
+	 *  Verifies that search works correctly when a fragment of a word doesnt exist past the edge
+	 */
+	public void testFailOffEdge () {
+		// Note: this grid is 1x2x3 in size
+		final char[][][] grid = new char[][][] { { { 'a', 'b', 'c' },
+				{ 'd', 'f', 'e' } } };
+		final int[][] location = _wordSearch.search(grid, "fender");
+		assertNull(location);
+	}
+
+	@Test
+	/**
+	 *  Verifies that search respects bounds in a tiny grid that is effectively 2D. Assuming the search goes L to R and top to bottom
+	 */
+	public void testSearchOutOfBoundsTopLeftToBottomRight () {
+		// Note: this grid is 1x2x3 in size
+		final char[][][] grid = new char[][][] { { { 'c', 'c', 'a' },
+				{ 'c', 'a', 't' } } };
+		final int[][] location = _wordSearch.search(grid, "cat");
+		assertNotNull(location);
+	}
+
+	@Test
+	/**
+	 *  Verifies that search respects bounds in a tiny grid that is effectively 2D. Assuming the search goes R to L and bottom to top
+	 */
+	public void testSearchOutOfBoundsBottomRightTopLeft () {
+		// Note: this grid is 1x2x3 in size
+		final char[][][] grid = new char[][][] { { { 't', 'a', 'c' },
+				{ 'a', 'c', 'c' } } };
+		final int[][] location = _wordSearch.search(grid, "cat");
+		assertNotNull(location);
+	}
+
+
+
 
 	@Test
 	/**
